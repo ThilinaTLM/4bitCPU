ec1572d50f071ae5cabf862a3307a1dc33db9422
ThilinaTLM <thilinalakshanmail@gmail.com>
2020-01-06 20:25:17 +0530

    Finished ADD_SUB_4bit.vhd

--- Lab9/Lab9.srcs/sources_1/new/ADD_SUB_4bit.vhd
+++ Lab9/Lab9.srcs/sources_1/new/ADD_SUB_4bit.vhd
@@ -1,47 +1,41 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 01/06/2020 07:59:16 PM
--- Design Name: 
--- Module Name: ADD_SUB_4bit - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity ADD_SUB_4bit is
     Port ( A : in STD_LOGIC_VECTOR (3 downto 0);
            B : in STD_LOGIC_VECTOR (3 downto 0);
-           Cin : in STD_LOGIC;
-           D : out STD_LOGIC_VECTOR (3 downto 0);
-           Cout : out STD_LOGIC);
+           C_in : in STD_LOGIC;
+           Sel : in STD_LOGIC; -- if 0 then A + B, if 1 then A - B
+           S : out STD_LOGIC_VECTOR (3 downto 0);
+           C_out : out STD_LOGIC);
 end ADD_SUB_4bit;
 
 architecture Behavioral of ADD_SUB_4bit is
-
+    component RCA_4bit
+        Port(
+            A : in  STD_LOGIC_VECTOR (3 downto 0);
+            B : in  STD_LOGIC_VECTOR (3 downto 0);
+            C_in : in  STD_LOGIC;
+            S : out  STD_LOGIC_VECTOR (3 downto 0);
+            C_out : out  STD_LOGIC
+        );
+    end component;
+    
+    Signal newB : STD_LOGIC_VECTOR (3 downto 0);
 begin
-
-
+    newB(0) <= B(0) xor Sel;
+    newB(1) <= B(1) xor Sel;
+    newB(2) <= B(2) xor Sel;
+    newB(3) <= B(3) xor Sel;
+    
+    RCA : RCA_4bit
+        Port map (
+            A => A,
+            B => newB,
+            C_in => Sel,
+            S => S,
+            C_out => C_out
+        );
+    
 end Behavioral;
